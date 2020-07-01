0cd50b4274022d3606e9a202993cd91f27ff8a98
Joshua Abraham <sinisterpatrician@gmail.com>
2020-07-01 16:15:08 -0400

    Use log crate for debug printing; Add ExtentHeader impl

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ use std::fs::File;
 use std::io::{Read, Seek, SeekFrom};
 use byteorder::{LittleEndian, ReadBytesExt};
 use failure::{Error, Fail};
+use log::info;
 
 #[derive(Debug, Fail)]
 pub enum VmdkError {
@@ -14,6 +15,7 @@ pub enum VmdkError {
     ParseError,
 }
 
+#[derive(Debug)]
 pub struct ExtentHeader {
     /// The header signature "KDMV"
     pub magicNumber: u32,
@@ -48,8 +50,85 @@ pub struct ExtentHeader {
     pub doubleEndLineChar: u8,
     /// Compression method
     pub compressAlgorithm: u16,
-    /// Padding
-    pub pad: [u8; 433],
+}
+
+impl ExtentHeader {
+    pub fn new<R: Read>(mut reader: R) -> Result<Self, Error> {
+        let magic = reader.read_u32::<LittleEndian>()?;
+        info!("Magic: 0x{:x}", magic);
+        if magic != EXTENT_MAGIC {
+            return Err(VmdkError::ParseError.into());
+        }
+
+        let version = reader.read_u32::<LittleEndian>()?;
+        info!("Version: 0x{:x}", version);
+        if version != EXTENT_VERSION {
+            return Err(VmdkError::ParseError.into());
+        }
+
+        let flags = reader.read_u32::<LittleEndian>()?;
+        info!("Flags: 0x{:x}", flags);
+
+        let capacity = reader.read_u64::<LittleEndian>()?;
+        info!("Capacity: 0x{:x}", capacity);
+
+        let grain_size = reader.read_u64::<LittleEndian>()?;
+        info!("Grain Size: 0x{:x}", grain_size);
+
+        let desc_offset = reader.read_u64::<LittleEndian>()?;
+        info!("Descriptor offset: 0x{:x}", desc_offset);
+
+        let desc_size = reader.read_u64::<LittleEndian>()?;
+        info!("Descriptor size: 0x{:x}", desc_size);
+
+        let gte_per_gt = reader.read_u32::<LittleEndian>()?;
+        info!("GTE's per GT: 0x{:x}", gte_per_gt);
+
+        let rgd_offset = reader.read_u64::<LittleEndian>()?;
+        info!("Redundant GD offset: 0x{:x}", rgd_offset);
+
+        let gd_offset = reader.read_u64::<LittleEndian>()?;
+        info!("GD Offset: 0x{:x}", gd_offset);
+
+        let meta_overhead = reader.read_u64::<LittleEndian>()?;
+        info!("Metadata overhead: 0x{:x}", meta_overhead);
+
+        let dirty_shutdown = reader.read_u8()?;
+        info!("Dirty shutdown: 0x{:x}", dirty_shutdown);
+
+        let eol_char = reader.read_u8()?;
+        info!("Single EOL Character: 0x{:x}", eol_char);
+
+        let non_eol_char = reader.read_u8()?;
+        info!("Non EOL Char: 0x{:x}", non_eol_char);
+
+        let dbl_eol_char = reader.read_u8()?;
+        info!("Double EOL Char: 0x{:x}", dbl_eol_char);
+
+        let compress_method = reader.read_u16::<LittleEndian>()?;
+        info!("Compression Algo: 0x{:x}", compress_method);
+
+        let ext = ExtentHeader {
+            magicNumber: magic,
+            version: version,
+            flags: flags,
+            capacity: capacity,
+            grainSize: grain_size,
+            descriptorOffset: desc_offset,
+            descriptorSize: desc_size,
+            numGTEsPerGT: gte_per_gt,
+            rgdOffset: rgd_offset,
+            gdOffset: gd_offset,
+            overHead: meta_overhead,
+            uncleanShutdown: dirty_shutdown,
+            singleEndLineChar: eol_char,
+            nonEndLineChar: non_eol_char,
+            doubleEndLineChar: dbl_eol_char,
+            compressAlgorithm: compress_method,
+        };
+
+        Ok(ext)
+    }
 }
 
 pub struct Vmdk {
@@ -58,64 +137,14 @@ pub struct Vmdk {
 }
 
 impl Vmdk {
+    // TODO: make the input generic over R: Read
     pub fn new<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
         let mut file = File::open(path)?;
 
         // Extent Header
         file.seek(SeekFrom::Start(0))?;
-        let magic = file.read_u32::<LittleEndian>()?;
-        eprintln!("Magic: 0x{:x}", magic);
-        if magic != EXTENT_MAGIC {
-            return Err(VmdkError::ParseError.into());
-        }
-
-        let version = file.read_u32::<LittleEndian>()?;
-        eprintln!("Version: 0x{:x}", version);
-        if version != EXTENT_VERSION {
-            return Err(VmdkError::ParseError.into());
-        }
-
-        let flags = file.read_u32::<LittleEndian>()?;
-        eprintln!("Flags: 0x{:x}", flags);
-
-        let capacity = file.read_u64::<LittleEndian>()?;
-        eprintln!("Capacity: 0x{:x}", capacity);
-
-        let grain_size = file.read_u64::<LittleEndian>()?;
-        eprintln!("Grain Size: 0x{:x}", grain_size);
-
-        let desc_offset = file.read_u64::<LittleEndian>()?;
-        eprintln!("Descriptor offset: 0x{:x}", desc_offset);
-
-        let desc_size = file.read_u64::<LittleEndian>()?;
-        eprintln!("Descriptor size: 0x{:x}", desc_size);
-
-        let gte_per_gt = file.read_u32::<LittleEndian>()?;
-        eprintln!("GTE's per GT: 0x{:x}", gte_per_gt);
-
-        let rgd_offset = file.read_u64::<LittleEndian>()?;
-        eprintln!("Redundant GD offset: 0x{:x}", rgd_offset);
-
-        let gd_offset = file.read_u64::<LittleEndian>()?;
-        eprintln!("GD Offset: 0x{:x}", gd_offset);
-
-        let meta_overhead = file.read_u64::<LittleEndian>()?;
-        eprintln!("Metadata overhead: 0x{:x}", meta_overhead);
-
-        let dirty_shutdown = file.read_u8()?;
-        eprintln!("Dirty shutdown: 0x{:x}", dirty_shutdown);
-
-        let eol_char = file.read_u8()?;
-        eprintln!("Single EOL Character: 0x{:x}", eol_char);
-
-        let non_eol_char = file.read_u8()?;
-        eprintln!("Non EOL Char: 0x{:x}", non_eol_char);
-
-        let dbl_eol_char = file.read_u8()?;
-        eprintln!("Double EOL Char: 0x{:x}", dbl_eol_char);
-
-        let compress_method = file.read_u16::<LittleEndian>()?;
-        eprintln!("Compression Algo: 0x{:x}", compress_method);
+        let extent_header = ExtentHeader::new(&mut file)?;
+        eprintln!("Ext: {:?}", extent_header);
 
         // Embedded Descriptor
         file.seek(SeekFrom::Start(512))?;
